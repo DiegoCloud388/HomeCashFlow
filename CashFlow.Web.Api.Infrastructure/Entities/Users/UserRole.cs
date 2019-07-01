9a9a41ef3278e34f129d89bab13289eef54ebf2a
Lukas Vitek <49225901+DiegoCloud388@users.noreply.github.com>
2019-07-01 09:02:19 +0200

    Create Entity

---
+++ CashFlow.Web.Api.Infrastructure/Entities/Users/UserRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashPrototype_v2._2.Web.Api.Infrastructure.Entities.Users
+{
+    public class UserRole : IdentityRole<int>
+    {
+    }
+}
