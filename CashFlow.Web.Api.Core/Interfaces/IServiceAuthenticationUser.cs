0eee1b1752822584ecb5183937444ba9724535e2
Lukas Vitek <49225901+DiegoCloud388@users.noreply.github.com>
2019-07-01 08:57:20 +0200

    Create Interfaces

---
+++ CashFlow.Web.Api.Core/Interfaces/IServiceAuthenticationUser.cs
@@ -0,0 +1,18 @@
+﻿using CashPrototype_v2._2.Web.Api.Core.DTO;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashPrototype_v2._2.Web.Api.Core.Interfaces
+{
+    public interface IServiceAuthenticationUser
+    {
+        Task<IdentityResult> RegistrationUser(UserDTO userDTO);
+
+        Task LogInUser(UserDTO userDTO);
+
+        Task LogOutUser(UserDTO userDTO);
+    }
+}
---
+++ CashFlow.Web.Api.Core/Interfaces/IServicesCRUD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashPrototype_v2._2.Web.Api.Core.Interfaces
+{
+    public interface IServicesCRUD<TEntityDTO> where TEntityDTO : class
+    {
+        Task<List<TEntityDTO>> GetAllItems();
+
+        Task<TEntityDTO> GetItemById(int id);
+
+        Task InsertItem(TEntityDTO entityDTO);
+
+        Task UpdateItem(TEntityDTO entityDTO);
+
+        Task DeleteItem(int id);
+    }
+}
